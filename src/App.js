e6e23c71bfa0b5ed3a1aab9b64e53e39d6c5677b
Olly Holovchenko <oleh.holo@gmail.com>
2022-03-30 21:19:43 +0100

    Create react app and connect with API

---
+++ src/App.js
@@ -0,0 +1,35 @@
+
+// import './App.css';
+import axios from "axios";
+import React from "react";
+
+const baseURL = "http://localhost:3000/api/generate/prompt";
+
+function App() {
+  const [word, setWord] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      console.log(response.data)
+      setWord(response.data);
+    });
+  }, []);
+
+  if (!word) return null;
+
+  console.log(word)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello world!
+        </p>
+      <h1>{word.randomPrompt}</h1>
+      </header>
+      <button onClick={setWord}>Create Post</button>
+    </div>
+  );
+}
+
+export default App;
