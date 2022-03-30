b5eef086ef19d8d4588e464176a6e07bfd31f6f7
Olly Holovchenko <oleh.holo@gmail.com>
2022-03-30 22:47:48 +0100

    Start playing with components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
+import './App.css';
+import axios from 'axios';
+import React from 'react';
 
-// import './App.css';
-import axios from "axios";
-import React from "react";
-
-const baseURL = "http://localhost:3000/api/generate/prompt";
+const baseURL = 'http://localhost:3000/api/generate/prompt';
 
 function App() {
   const [word, setWord] = React.useState(null);
 
   React.useEffect(() => {
     axios.get(baseURL).then((response) => {
-      console.log(response.data)
+      console.log(response.data);
       setWord(response.data);
     });
   }, []);
 
   if (!word) return null;
 
-  console.log(word)
+  console.log(word);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello world!
-        </p>
-      <h1>{word.randomPrompt}</h1>
+        <p>Hello world!</p>
+        <h1>{word.randomPrompt}</h1>
       </header>
       <button onClick={setWord}>Create Post</button>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from "./App";
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Hello />
   </React.StrictMode>,
   document.getElementById('root')
 );
