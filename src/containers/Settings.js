6f04c5e5a1ddffafcc9523141ee7c4e15d130a2e
Olly Holovchenko <oleh.holo@gmail.com>
2022-05-04 20:22:42 +0100

    Remove timer and chips from settings.

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -1,29 +1,17 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
-import { Paper, FormControl, InputLabel, Select, MenuItem, Typography, Chip } from '@mui/material';
+import { FormControl, InputLabel, Select, MenuItem, Typography } from '@mui/material';
 import { GameModes, WordSets } from '../data/Settings';
 // import { GameModes, Timers, WordSets } from '../data/Settings';
 import { connect } from 'react-redux';
 import { selectGameMode } from '../actions/actions';
-
-const ListItem = styled('li')(({ theme }) => ({
-  margin: theme.spacing(0.5),
-}));
-
-
 
 const GameModesList = GameModes.map((mode, i) => (
   <MenuItem value={mode} key={i}>
     {mode}
   </MenuItem>
 ));
-// const TimersLists = Timers.map((timer, i) => (
-//   <MenuItem value={timer} key={i}>
-//     {timer}
-//   </MenuItem>
-// ));
 const WordSetsList = WordSets.map((wordSet, i) => (
   <MenuItem value={wordSet} key={i}>
     {wordSet}
@@ -31,32 +19,7 @@ const WordSetsList = WordSets.map((wordSet, i) => (
 ));
 
 const Settings = (props) => {
-  const [chipData, setChipData] = React.useState([
-    { key: 0, label: 'Angular' },
-    { key: 1, label: 'jQuery' },
-    { key: 2, label: 'Polymer' },
-    { key: 3, label: 'React'},
-    { key: 4, label: 'Vue.js'},
-  ]);
-
-  const [selectedChips, setSelected] = useState([])
-
-  const handleSelect = (chipToSelect) => () => {
-    console.log('OLD SELECTED', selectedChips)
-    // console.log(chipToSelect)
-    setChipData([...chipData])
-    // setSelected([...selectedChips, chipToSelect])
-    if (selectedChips.find((e) => e === chipToSelect)) {
-      const index = selectedChips.findIndex((e) => e === chipToSelect);
-      let arr = [...selectedChips];
-      arr.splice(index, 1);
-      setSelected(arr);
-    } else {
-      setSelected([...selectedChips, chipToSelect]);
-    }
-    console.log('NEW SELECTED', selectedChips)
-    // setChipData((chips) => chips.filter((chip) => chip.key !== chipToSelect.key));
-  };
+  
   const [gameMode, setGameMode] = useState('');
   const [wordSet, setWordSet] = useState('');
 
@@ -64,9 +27,6 @@ const Settings = (props) => {
     setGameMode(event.target.value);
     dispatch(selectGameMode(event.target.value));
   };
-  // const handleTimerChange = (event) => {
-  //   setTimer(event.target.value);
-  // };
   const handleWordSetChange = (event) => {
     setWordSet(event.target.value);
   };
@@ -102,53 +62,13 @@ const Settings = (props) => {
           {WordSetsList}
         </Select>
       </FormControl>
-      <Paper
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        flexWrap: 'wrap',
-        listStyle: 'none',
-        p: 0.5,
-        m: 0,
-      }}
-      component="ul"
-    >
-      {chipData.map((chip) => {
-
-      const selected = selectedChips.includes(chip);
-
-        return (
-          <ListItem key={chip.key}>
-            <Chip
-              label={chip.label}
-              color={selected ? 'primary' : 'default'}
-              selected={selected}
-              onClick={handleSelect(chip)}
-              clickable
-            />
-          </ListItem>
-        );
-      })}
-    </Paper>
-      {/* <FormControl sx={{ m: 1, minWidth: 200 }}>
-        <InputLabel id="demo-simple-select-helper-label">Timer</InputLabel>
-        <Select
-          labelId="demo-simple-select-helper-label"
-          id="demo-simple-select-helper"
-          value={timer}
-          label="Timer"
-          onChange={handleTimerChange}
-        >
-          {TimersLists}
-        </Select>
-      </FormControl> */}
+      
     </>
   );
 };
 
 Settings.propTypes = {
   dispatch: PropTypes.func.isRequired,
-  // classes: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state) => ({
