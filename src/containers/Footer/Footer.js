574431a41e4d5ccf0cae477a5d408c20259b1cea
Olly Holovchenko <oleh.holo@gmail.com>
2022-05-25 12:53:19 +0100

    Restructure components.

---
+++ src/containers/Footer/Footer.js
@@ -0,0 +1,148 @@
+import * as React from 'react';
+import {
+  AppBar,
+  Avatar,
+  Toolbar,
+  Tooltip,
+  Typography,
+  Container,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Button
+} from '@mui/material';
+import AdbIcon from '@mui/icons-material/Adb';
+
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const pages = ['Products', 'Pricing', 'Blog'];
+
+const Header = () => {
+  return (
+    <AppBar
+      position="static"
+    >
+      <Container maxWidth="md">
+        <Toolbar disableGutters>
+          {/* LOGO */}
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontWeight: 700,
+              color: 'inherit',
+              textDecoration: 'none'
+            }}
+          >
+            Things To Draw
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              {/* <MenuIcon /> */}
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              // anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left'
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left'
+              }}
+              // open={Boolean(anchorElNav)}
+              // onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' }
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page}
+                  // onClick={handleCloseNavMenu}
+                >
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none'
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                // onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              {/* <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}> */}
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              {/* </IconButton> */}
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              // anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+            >
+              {settings.map((setting) => (
+                <MenuItem
+                  key={setting}
+                >
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Header;
---
+++ src/containers/SettingsBox/SettingsBox.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { MenuItem, Typography } from '@mui/material';
+import { gameModesWordPacks } from '../../data/Settings';
+import { selectGameMode } from '../../feature/gameModeSlice';
+import { selectWordPack } from '../../feature/wordPackSlice';
+import GameModeSelector from '../../components/GameModeSelector/GameModeSelector';
+import WordPackSelector from '../../components/WordPackSelector/WordPackSelector';
+import { useDispatch, useSelector } from 'react-redux';
+
+const SettingsBox = () => {
+  const gameMode = useSelector((state) => state.gameMode.value);
+  const wordPack = useSelector((state) => state.wordPack.value);
+  const dispatch = useDispatch();
+
+  const GameModesList = Object.keys(gameModesWordPacks).map((mode, i) => (
+    <MenuItem value={mode} key={i}>
+      {mode}
+    </MenuItem>
+  ));
+
+  const WordPacksList = gameModesWordPacks[gameMode].map((wordPack, i) => (
+    <MenuItem value={wordPack} key={i}>
+      {wordPack}
+    </MenuItem>
+  ));
+
+  const handleGameModeChange = (event) => {
+    dispatch(selectGameMode(event.target.value));
+  };
+  const handleWordPackChange = (event) => {
+    dispatch(selectWordPack(event.target.value));
+  };
+
+  return (
+    <>
+      <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+        Settings
+      </Typography>
+      <GameModeSelector
+        value={gameMode}
+        onChange={handleGameModeChange}
+        gameModesList={GameModesList}
+      />
+      <WordPackSelector
+        value={wordPack}
+        onChange={handleWordPackChange}
+        wordPackList={WordPacksList}
+      />
+    </>
+  );
+};
+
+export default SettingsBox;
